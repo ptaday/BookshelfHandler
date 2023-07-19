76d77afe5845758bc03f2a53aff41229f9c5e1c1
Pushkar Taday <124524200+ptaday@users.noreply.github.com>
2023-07-19 01:48:11 -0400

    Update FullShelfException.java

--- FullShelfException.java
+++ FullShelfException.java
@@ -1,10 +1,4 @@
-/**
- * @author Pushkar Taday
- * SBU ID:114375166
- * Recitation:04
- */
 
-package Homework1;
 
 /**
  * This class represents customised FullShelfException which handles the exception when thrown.
